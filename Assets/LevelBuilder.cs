d0b46f588b9009ce84148e7941090750e8478c06
Thirgen <22317125+thirgen@users.noreply.github.com>
2021-06-11 23:32:41 +0100

    LevelBuilder now stores the loaded level array

--- Assets/LevelBuilder.cs
+++ Assets/LevelBuilder.cs
@@ -5,8 +5,9 @@ public class LevelBuilder : MonoBehaviour
 {
     public TextAsset level;
     public TileType[] Tiles;
+    public char[,] tileMap;
+    private static readonly char NULL_CHAR = '!';
 
-    // Start is called before the first frame update
     void Start()
     {
         if (level == null) return;
@@ -19,19 +20,53 @@ public class LevelBuilder : MonoBehaviour
         string text = level.text;
         string[] lines = Regex.Split(text, "\n|\r\n");
 
+        CalcLevelBounds(lines);
+        PopulateTilemap(lines);
+        Build();
+    }
+
+    private void CalcLevelBounds(string[] lines)
+    {
+        int xBound = lines.Length;
+        int zBound = 0;
         for (int x = 0; x < lines.Length; x++)
+        {
+            int currentLength = lines[x].Length;
+            if (currentLength > zBound)
+                zBound = currentLength;
+        }
+        tileMap = new char[xBound, zBound];
+    }
+
+    private void PopulateTilemap(string[] lines)
+    {
+        for (int x = 0; x < tileMap.GetLength(0); x++)
+        {
+            string currentLine = lines[x];
+            for (int z = 0; z < tileMap.GetLength(1); z++)
+            {
+                tileMap[x, z] = (z >= currentLine.Length) ? NULL_CHAR : currentLine[z];
+            }
+        }
+    }
+
+    private void Build()
+    {
+        for (int x = 0; x < tileMap.GetLength(0); x++)
         {
             GameObject currentParent = new GameObject($"{x}");
             currentParent.transform.SetParent(transform);
-            string currentLine = lines[x];
-            for (int z = 0; z < currentLine.Length; z++)
+            for (int z = 0; z < tileMap.GetLength(1); z++)
             {
+                char currentChar = tileMap[x, z];
+                if (currentChar == NULL_CHAR) continue;
+
                 var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 go.transform.SetParent(currentParent.transform);
                 go.transform.position = new Vector3(z, -1f, -x);
                 go.name = $"{z}";
 
-                Material m = GetMaterial(currentLine[z]);
+                Material m = GetMaterial(currentChar);
                 if (m != null)
                     go.GetComponent<Renderer>().material = m;
             }
