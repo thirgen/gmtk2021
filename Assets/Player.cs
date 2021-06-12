c2d2fa8cb25aca180aba4c4b1eb762e93f3a174d
Thirgen <22317125+thirgen@users.noreply.github.com>
2021-06-12 10:18:47 +0100

    added basic dash

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,27 +1,45 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
     /// <summary>
-    /// Time in seconds in between movements along the Z axis (left/right)
+    /// Time in seconds in between movements along the Z axis (left/right).
     /// </summary>
-    [Tooltip("Time in seconds in between movements along the Z axis (left/right)")]
+    [Tooltip("Time in seconds in between movements along the Z axis (left/right).")]
     public float ZDelay = 0.025f;
+
+    /// <summary>
+    /// The minimum time in seconds in between dash attacks.
+    /// </summary>
+    [Tooltip("The minimum time in seconds in between dash attacks.")]
+    public float DashCooldown = 0.25f;
 
     private KeyCode _upKey = KeyCode.W;
     private KeyCode _downKey = KeyCode.S;
     private KeyCode _leftKey = KeyCode.A;
     private KeyCode _rightKey = KeyCode.D;
+    private KeyCode _upDashKey = KeyCode.UpArrow;
+    private KeyCode _downDashKey = KeyCode.DownArrow;
+    private KeyCode _leftDashKey = KeyCode.LeftArrow;
+    private KeyCode _rightDashKey = KeyCode.RightArrow;
     private LevelBuilder _levelBuilder;
+    private Animator _animator;
 
     /// <summary>
-    /// The time of the last successful movement along the Z axis
+    /// The time of the last successful movement along the Z axis.
     /// </summary>
     private float _lastZMove = 0f;
+
+    /// <summary>
+    /// The time of the last successful dash.
+    /// </summary>
+    private float _lastDash = 0f;
 
     void Start()
     {
         _levelBuilder = LevelBuilder.current;
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -55,19 +73,46 @@ public class Player : MonoBehaviour
             desiredPosition -= zMovement;
         }
 
+
+        // Dash will move the player 1.5f along the z axis. No dashing up/down
+        bool isDashing = false;
+        Vector3 zDash = Vector3.forward * 2f;
+        if (Input.GetKeyDown(_rightDashKey) && CanDash)
+        {
+            desiredPosition += zDash;
+            _lastDash = Time.time;
+            isDashing = true;
+        }
+        if (Input.GetKeyDown(_leftDashKey) && CanDash)
+        {
+            desiredPosition -= zDash;
+            _lastDash = Time.time;
+            isDashing = true;
+        }
+
         if (transform.position != desiredPosition)
         {
             if (_levelBuilder.IsValidMove(desiredPosition))
             {
                 transform.position = desiredPosition;
                 _lastZMove = Time.time;
+                if (isDashing)
+                {
+                    // play dash animation
+                    _animator.SetTrigger("Dash");
+                }
             }
             // TODO else play bump animation?
         }
     }
 
     /// <summary>
-    /// Returns true if it has been longer than ZDelay since the last attempted move along the Z axis
+    /// Returns true if it has been longer than <see cref="ZDelay"/> since the last successful move along the Z axis
     /// </summary>
     private bool CanMoveZ => Time.time - _lastZMove > ZDelay;
+
+    /// <summary>
+    /// Returns true if it has been longer than <see cref="DashCooldown"/> since the last attempted dash
+    /// </summary>
+    private bool CanDash => Time.time - _lastDash > DashCooldown;
 }
